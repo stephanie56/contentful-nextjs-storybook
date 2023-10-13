735744747b4c6a03db1b1f56bbd327b0bbaa178c
Stephanie Zeng <Stephanie.Zeng@jetblue.com>
2023-10-13 13:31:45 -0400

    Add storybook template generator

---
+++ generator/plopfile.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line func-names
+module.exports = function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  plop.setGenerator('component', {
+    description: 'creates a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the component name (e.g. checkbox):',
+      },
+    ],
+    actions: (answers) => {
+      const actionsArray = [
+        {
+          type: 'add',
+          path: '../components/{{properCase name}}/index.ts',
+          templateFile: './componentTemplate/index.hbs',
+        },
+        {
+          type: 'add',
+          path: '../components/{{properCase name}}/{{properCase name}}.tsx',
+          templateFile: './componentTemplate/component.hbs',
+        },
+        {
+          type: 'add',
+          path: '../components/{{properCase name}}/{{properCase name}}.stories.ts',
+          templateFile: './componentTemplate/stories.hbs',
+        },
+      ];
+
+      return actionsArray;
+    },
+  });
+};
